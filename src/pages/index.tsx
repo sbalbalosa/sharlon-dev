efe74dfe0fe3ce5bea5903c76ee9149b9ddca94d
Sharlon Mangisel Balbalosa <sbalbalosa@deloitte.com>
2021-09-13 22:09:22 +0800

    added hero text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
 import React from "react"
 
+import Header from "../components/Header"
+
 export default function Home() {
-  return <div className="text-red-500">Hello world!!</div>
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-gray-100 text-6xl">Hello, Sharlon Balbalosa</h1>
+        <p className="text-gray-700">
+          I am a frontend developer with experience working with web and mobile
+        </p>
+        {/* <article>Transforming business ideas into working software</article>
+
+        <section>
+          <ul>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>TyeScript</li>
+            <li>HTML, CSS &amp; SASS</li>
+          </ul>
+        </section>
+
+        <section>
+          <ul>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>TyeScript</li>
+            <li>HTML, CSS &amp; SASS</li>
+          </ul>
+        </section> */}
+      </main>
+    </>
+  )
 }
